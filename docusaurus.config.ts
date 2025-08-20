43b956a6801c992694b30d260603e91dabb8c0a5
Gusde Widnyana <widnyana.bagus@gmail.com>
2025-08-20 17:02:41 +0800

    feat: Add local search plugin and update primary color theme
    Integrated 'docusaurus-plugin-search-local' for improved on-site search functionality and enabled search term highlighting. Updated the primary color palette in custom.css for both light and dark modes to a blue scheme for better branding consistency.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
@@ -66,6 +66,16 @@ const config: Config = {
           customCss: './src/css/custom.css',
         },
       } satisfies Preset.Options,
+    ],
+  ],
+
+  plugins: [
+    [
+      require.resolve('docusaurus-plugin-search-local'),
+      {
+        hashed: true,
+        highlightSearchTermsOnTargetPage: true
+      },
     ],
   ],
 
